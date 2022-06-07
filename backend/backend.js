04b53fe20f2be75a7deb71ff32cf6f497ff07f08
Sahra Zhou <63219465+Sahra-Zhou@users.noreply.github.com>
2022-06-06 23:48:03 -0700

    Add files via upload

---
+++ backend/backend.js
@@ -0,0 +1,36 @@
+//main
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3000;
+const{ json } = require("express");
+const mysql = require('mysql');
+  
+const con = mysql.createConnection({
+    host: "mysql.labthreesixfive.com",
+    user: "group6a",
+    password: "NR^DH\"e<CV8*j<@h",
+    database: "group6a"
+});
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/:Dashboard", async (req, res) => {
+    con.query(`select * from Survey;`, (err, ret) =>{
+        if(err) throw err;
+          res.send(ret);
+    });
+});
+
+app.get("/:ShowSurveys", async (req, res) => {
+    const SurveyId = req.params["SurveyId"];
+    con.query(`select * from SurveyResponse where SurveyId = ${SurveyId};`, (err, result) =>{
+        if(err) throw err;
+          res.send(result);
+    })
+});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
