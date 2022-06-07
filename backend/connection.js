ec85cf7a38e7e2a5d61891b7253e9c4443979c70
Sahra Zhou <tongzhou.chengdu@hotmail.com>
2022-06-06 21:02:52 -0700

    add functions to get data from database

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,6 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
   
-var con = mysql.createConnection({
+const con = mysql.createConnection({
     host: "mysql.labthreesixfive.com",
     user: "group6a",
     password: "NR^DH\"e<CV8*j<@h",
@@ -13,7 +13,7 @@ var con = mysql.createConnection({
      console.log("Connected!");
      });*/
 //create connection and switch to group database
-con.connect(function (err) {
+/*con.connect(function (err) {
    if (err) throw err;
    console.log("Connected!");
 
@@ -22,4 +22,58 @@ con.connect(function (err) {
        if (err) throw err;
        console.log(result);
    });
+});*/
+con.query(`select count(*) from Survey;`, (err, res) =>{
+    if(err) throw err;
+        console.log(res);
 });
+con.query(`select * from Survey;`, (err, res) =>{
+    if(err) throw err;
+        console.log(res);
+});
+/*
+async function getSurvey(){
+    const survey = con.query(`select * from Survey;`);
+    return survey;
+}
+async function getQuestions(surveyId){
+    const questions = con.query(`select * from Questions where Survey = ${surveyId}`);
+    return questions;
+}
+async function getPossibleAnswer(questionId, surveyId){
+    const qtype = con.query(`select qtype from Questions where Survey = ${surveyId} 
+        and QuestionId = ${questionId}`);
+    if (qtype = 0){
+        return con.query(`select * from Possibilities where qtype = 1 and QuestionNo = ${questionId}
+        and survey = ${surveyId};`);}
+    else {
+        return con.query(`select * from Possiblities where qtype = 0;`);
+    }
+}
+async function countResponses(surveyId){
+    const counts = con.query(`select count(*) from SurveyResponse where SurveyId = ${surveyId};`);
+}
+//get all responses for a survey
+async function getAllSurveyRes(SurveyId){
+    const responses = con.query(`select * from SurveyResponse where SurveyId = ${SurveyId};`);
+    return responses;
+}
+//get info for one survey response
+async function getResponse(resId){
+    const response = con.query(`select * from SurveyResponse where SurvResp = ${resId};`);
+    return response;
+}
+//get responses to questions for one survey response
+async function getIndividualRes(resId){
+    const indResponse = con.query(`select * from Responses where SurvResp = ${resId};`);
+    return indResponse
+}
+
+module.export{
+    getSurvey,
+    countResponses,
+    getQuestions,
+    getAllSurveyRes,
+    getResponses,
+    getIndividualRes
+};*/
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
 //main
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 5000;
+
+const{ json } = require("express");
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {});
+
