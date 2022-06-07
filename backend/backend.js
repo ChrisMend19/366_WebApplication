bc47971ebb66567a4132fa4146968d6822b31c25
Shiro <shiro@damedane.local>
2022-06-07 16:14:29 -0700

    try to show and change status

--- backend/backend.js
+++ backend/backend.js
@@ -17,15 +17,40 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get("/:Dashboard", async (req, res) => {
+app.get("/Dashboard", async (req, res) => {
     con.query(`select * from Survey;`, (err, ret) =>{
         if(err) throw err;
           res.send(ret);
+          //console.log(ret);
     });
 });
-
-app.get("/:ShowSurveys", async (req, res) => {
-    const SurveyId = req.params["SurveyId"];
+app.post("/Dashboard", async (req, res) => {
+    const id = req.params["surveyId"];
+    const status = req.params["status"];
+    if (status == 1){
+        con.query(`update table Survey set Status = false where SurveyId = ${id};`, (err, ret) =>{
+            if(err) throw err;
+              res.status(201).end();
+        });
+    }
+    else{
+        con.query(`update table Survey set Status = false where SurveyId = ${id};`, (err, ret) =>{
+            if(err) throw err;
+              res.status(201).end();
+        });
+    }
+});
+//get all questions in a survey
+app.get("/ShowSurveys/:survey", async (req, res) => {
+    const id = req.params["survey"];
+    con.query(`select * from Questions where Survey = ${id};`, (err, result)=>{
+        if(err) throw err
+        res.status(201).send(result);
+    })
+});
+//get survey responses on responses page
+app.get("/SurveyResponses/:survey", async (req, res) => {
+    const SurveyId = req.params["survey"];
     con.query(`select * from SurveyResponse where SurveyId = ${SurveyId};`, (err, result) =>{
         if(err) throw err;
           res.status(201).send(result);
